afbd13434fa6c2445e685d3e169baaef477976b0
rosey <rsarnataro@tulane.edu>
2023-11-09 14:37:49 -0600

    Add project files.

---
+++ api/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apicalls
+{
+    [Route("api/users")]
+    public class ApiController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Testing()
+        {
+            return Ok("hi");
+        }
+    }
+}
